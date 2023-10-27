536d89d052ba40fb68d5a744756bdce2ae3815d0
youngjaekwon <gczzga@gmail.com>
2023-10-27 18:06:14 +0900

    User DB를 가져오는 dependency를 추가

--- app/api/dependencies.py
+++ app/api/dependencies.py
@@ -1,9 +1,16 @@
 from typing import AsyncGenerator
 
+from app.infrastructure.db.models import User
 from app.infrastructure.db.session import async_session_maker
+from fastapi import Depends
+from fastapi_users.db import SQLAlchemyUserDatabase
 from sqlalchemy.ext.asyncio import AsyncSession
 
 
 async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
     async with async_session_maker() as session:
         yield session
+
+
+async def get_user_db(session: AsyncSession = Depends(get_async_session)):
+    yield SQLAlchemyUserDatabase(session, User)
